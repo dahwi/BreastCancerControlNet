46b17697f835ecd247a7dc8e4ce9eb844b8615c4
diyaparm <diyavparmar@gmail.com>
2024-12-01 23:47:44 +0000

    classifier for sd

--- scripts/stableDiffusion_augmentation.py
+++ scripts/stableDiffusion_augmentation.py
@@ -3,7 +3,8 @@ import yaml
 from dataset.dataset_helper import get_dataset
 from dataset.ultrasound_breast_dataset import UltrasoundBreastDataset
 from model.stable_diffusion_utils import fine_tune
-
+from torch.utils.data import ConcatDataset
+from model.classifier_utils import run
 
 def main(config_file_path):
     # Load configuration
@@ -14,7 +15,12 @@ def main(config_file_path):
     print(f"Using device: {device}")
 
     dataset = get_dataset(UltrasoundBreastDataset, config['data_dir'], 256, 256, [0.5], [0.5], augment=False)
-    fine_tune(config, dataset, device)
+   # fine_tune(config, dataset, device)
+    augmented_dataset = get_dataset(UltrasoundBreastDataset, config['sd_ft_agument_dir'], 256, 256, [0.5], [0.5], augment=False)
+
+    combined_dataset = ConcatDataset([dataset, augmented_dataset])
+
+    run('stable_diffusion', combined_dataset, config, device)
 
 
 if __name__ == '__main__':
