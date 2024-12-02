87d566b005a43723e86a43a17ac1dd5eca0b3782
dahwi <dahwi.kim96@gmail.com>
2024-12-02 23:57:06 +0000

    fix typos

--- scripts/stableDiffusion_augmentation.py
+++ scripts/stableDiffusion_augmentation.py
@@ -17,7 +17,7 @@ def main(config_file_path):
     dataset = get_dataset(UltrasoundBreastDataset, config['data_dir'], 256, 256, [0.5], [0.5], augment=False)
     # for c in ["benign", "normal", "malignant"]:
         # fine_tune(config, dataset, device, c, wandb_log=True)
-    augmented_dataset = get_dataset(UltrasoundBreastDataset, config['sd_ft_augument_dir'], 256, 256, [0.5], [0.5], augment=False)
+    augmented_dataset = get_dataset(UltrasoundBreastDataset, config['sd_ft_augment_dir'], 256, 256, [0.5], [0.5], augment=False)
 
     combined_dataset = ConcatDataset([dataset, augmented_dataset])
 
